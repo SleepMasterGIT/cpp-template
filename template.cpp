afcb50688366f6a1197f940d88a67e836c5d6679
Dusan Brkic <brkicdusan05@gmail.com>
2020-11-27 20:09:29 +0100

    template

---
+++ template.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+#define ff first 
+#define ss second 
+#define mp make_pair
+
+#define pb push_back
+
+#define rep(i,n) for(auto i=0; i<(n); i++) 
+
+#define all(x) (x).begin(), (x).end() 
+
+using namespace std;
+
+typedef  map<string,int>         msi; 
+typedef  map<int,int>	         mii; 
+typedef  map<ll, ll>             mll; 
+typedef  map<char,int>           mci; 
+typedef  map<int,string>	     mis; 
+typedef  pair<int,int> 	         pii;
+typedef  pair<ll, ll> 	         pll;  
+typedef  pair<string, int>       psi; 
+typedef  pair<string, string>    pss; 
+typedef  vector<int> 	         vi;  
+typedef  vector<string> 	     vs; 
+typedef  vector<char>	         vc; 
+typedef  vector<bool>            vb; 
+typedef  vector< pii >           vii;
+typedef  vector< ll >            vll;
+typedef  vector<pii>             vpii;
+typedef  vector<pll>             vpll;
+typedef  vector<vi>              vvi;
+typedef  vector<vll>             vvll;
+
+
+const int mod = 1000000007;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
